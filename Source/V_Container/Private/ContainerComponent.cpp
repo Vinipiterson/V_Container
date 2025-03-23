4db7f7264b726bb5c3f7ea049dd024265101e6bb
Vinipiterson <Vinipiassini123@gmail.com>
2025-03-23 09:10:50 -0300

    Basic functionality working.

---
+++ Source/V_Container/Private/ContainerComponent.cpp
@@ -0,0 +1,249 @@
+﻿// Copyright Vinipi, 2025.
+
+
+#include "ContainerComponent.h"
+
+#include "Net/UnrealNetwork.h"
+#include "Net/Core/PushModel/PushModel.h"
+
+UContainerComponent::UContainerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	PrimaryComponentTick.bAllowTickOnDedicatedServer = false;
+
+	SetIsReplicatedByDefault(true);
+}
+
+void UContainerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UContainerComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	FDoRepLifetimeParams Parameters;
+	Parameters.bIsPushBased = true;
+
+	Parameters.Condition = COND_None;
+	Parameters.RepNotifyCondition = REPNOTIFY_Always;
+	DOREPLIFETIME_WITH_PARAMS_FAST(ThisClass, Elements, Parameters)
+}
+
+void UContainerComponent::TryAddElement(AActor* Element, int32 InIndex)
+{
+	if (GetOwnerRole() >= ROLE_Authority)
+	{
+		TryAddElement_Internal(Element, InIndex);
+	}
+	else
+	{
+		ServerTryAddElement(Element, InIndex);
+	}
+}
+
+void UContainerComponent::TryRemoveElement(AActor* Element, int32 InIndex)
+{
+	if (GetOwnerRole() >= ROLE_Authority)
+	{
+		TryRemoveElement_Internal(Element, InIndex);
+	}
+	else
+	{
+		ServerTryRemoveElement(Element, InIndex);
+	}
+}
+
+void UContainerComponent::TryRemoveElementByIndex(int32 InIndex)
+{
+	if (GetOwnerRole() >= ROLE_Authority)
+	{
+		TryRemoveElementByIndex_Internal(InIndex);
+	}
+	else
+	{
+		ServerTryRemoveElementByIndex(InIndex);
+	}
+}
+
+void UContainerComponent::TrySwapElement(int32 OldIndex, int32 NewIndex)
+{
+	if (GetOwnerRole() >= ROLE_Authority)
+	{
+		TrySwapElement_Internal(OldIndex, NewIndex);
+	}
+	else
+	{
+		ServerTrySwapElement(OldIndex, NewIndex);
+	}
+}
+
+bool UContainerComponent::HasElement(const AActor* InElement) const
+{
+	if (!InElement) return false;
+	
+	for (const auto& Element : Elements)
+	{
+		if (Element == InElement)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+AActor* UContainerComponent::GetElement(int32 InIndex) const
+{
+	if (InIndex == INDEX_NONE) return nullptr;
+	if (!Elements.IsValidIndex(InIndex)) return nullptr;
+
+	return Elements[InIndex];
+}
+
+TArray<AActor*> UContainerComponent::GetElements() const
+{
+	return Elements;
+}
+
+int32 UContainerComponent::GetElementIndex(const AActor* InElement) const
+{
+	if (!InElement) return INDEX_NONE;
+	
+	for (int i = 0; i < Elements.Num(); i++)
+	{
+		if (Elements[i] == InElement)
+		{
+			return i;
+		}
+	}
+
+	return INDEX_NONE;
+}
+
+// Internal use
+bool UContainerComponent::TryAddElement_Internal(AActor* Element, int32 InIndex)
+{
+	if (!Element) return false;
+	if (InIndex != INDEX_NONE && !Elements.IsValidIndex(InIndex)) return false;
+
+	if (InIndex == INDEX_NONE)
+	{
+		if (int Index = FindFirstAvailableIndex() != INDEX_NONE)
+		{
+			AddElement(Element, Index);
+			return true;
+		}
+	}
+
+	if (TryRemoveElement_Internal(Elements[InIndex], InIndex))
+	{
+		AddElement(Element, InIndex);
+		return true;
+	}
+	
+	return false;
+}
+
+bool UContainerComponent::TryRemoveElement_Internal(AActor* Element, int32 InIndex)
+{
+	if (!Element) return false;
+
+	if (!Elements[InIndex]) return false;
+
+	if (Elements[InIndex] == Element)
+	{
+		RemoveElement(InIndex);
+
+		return true;
+	}
+	
+	return false;
+}
+
+bool UContainerComponent::TryRemoveElementByIndex_Internal(int32 InIndex)
+{
+	if (!Elements[InIndex]) return false;
+
+	RemoveElement(InIndex);
+
+	return true;
+}
+
+bool UContainerComponent::TrySwapElement_Internal(int32 OldIndex, int32 NewIndex)
+{
+	if (OldIndex == INDEX_NONE || NewIndex == INDEX_NONE) return false;
+	if (!Elements.IsValidIndex(OldIndex) || !Elements.IsValidIndex(NewIndex)) return false;
+	
+	AActor* CurrentElement = Elements[OldIndex];
+	AActor* NewElement = Elements[NewIndex];
+
+	if (!CurrentElement || !NewElement) return false;
+	if (CurrentElement == NewElement) return false;
+
+	if (TryRemoveElement_Internal(CurrentElement, OldIndex))
+	{
+		return TryAddElement_Internal(NewElement, OldIndex);
+	}
+
+	return false;
+}
+
+AActor* UContainerComponent::AddElement(AActor* Element, int32 InIndex)
+{
+	TArray<AActor*> OldElements = Elements;
+	
+	Elements[InIndex] = Element;
+	MARK_PROPERTY_DIRTY_FROM_NAME(ThisClass, Elements, this);
+	OnRep_Elements(OldElements);
+
+	return Element;
+}
+
+void UContainerComponent::RemoveElement(int32 InIndex)
+{
+	TArray<AActor*> OldElements = Elements;
+
+	AActor* RemovedElement = Elements[InIndex];
+	
+	Elements[InIndex] = nullptr;
+	MARK_PROPERTY_DIRTY_FROM_NAME(ThisClass, Elements, this);
+	OnRep_Elements(OldElements);
+}
+
+void UContainerComponent::ServerTryAddElement_Implementation(AActor* Element, int32 InIndex)
+{
+	TryAddElement(Element, InIndex);
+}
+
+void UContainerComponent::ServerTryRemoveElement_Implementation(AActor* Element, int32 InIndex)
+{
+	TryRemoveElement(Element, InIndex);
+}
+
+void UContainerComponent::ServerTryRemoveElementByIndex_Implementation(int32 InIndex)
+{
+	TryRemoveElementByIndex(InIndex);
+}
+
+void UContainerComponent::ServerTrySwapElement_Implementation(int32 OldIndex, int32 NewIndex)
+{
+	TrySwapElement(OldIndex, NewIndex);
+}
+
+void UContainerComponent::OnRep_Elements(TArray<AActor*> OldElements)
+{
+	
+}
+
+int UContainerComponent::FindFirstAvailableIndex()
+{
+	for (int i = 0; i < Elements.Num(); i++)
+	{
+		if (Elements[i]) continue;
+
+		return i;
+	}
+	return INDEX_NONE;
+}
---
+++ Source/V_Container/Public/ContainerComponent.h
@@ -0,0 +1,83 @@
+﻿// Copyright Vinipi, 2025.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ContainerComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FContainerUpdatedSignature);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class V_CONTAINER_API UContainerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UContainerComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	//~Container
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "State", Replicated)
+	TArray<AActor*> Elements;
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FContainerUpdatedSignature OnContainerUpdated;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Container")
+	void TryAddElement(AActor* Element, int32 InIndex);
+	UFUNCTION(BlueprintCallable, Category = "Container")
+	void TryRemoveElement(AActor* Element, int32 InIndex);
+	UFUNCTION(BlueprintCallable, Category = "Container")
+	void TryRemoveElementByIndex(int32 InIndex);
+	UFUNCTION(BlueprintCallable, Category = "Container")
+	void TrySwapElement(int32 OldIndex, int32 NewIndex);
+	
+	UFUNCTION(BlueprintPure, Category = "Container")
+	bool HasElement(const AActor* InElement) const;
+	UFUNCTION(BlueprintPure, Category = "Container")
+	AActor* GetElement(int32 InIndex) const;
+	UFUNCTION(BlueprintPure, Category = "Container")
+	TArray<AActor*> GetElements() const;
+	UFUNCTION(BlueprintPure, Category = "Container")
+	int32 GetElementIndex(const AActor* InElement) const;
+
+protected:
+	/* Server only */
+	virtual bool TryAddElement_Internal(AActor* Element, int32 InIndex);
+	/* Server only */
+	virtual bool TryRemoveElement_Internal(AActor* Element, int32 InIndex);
+	/* Server only */
+	virtual bool TryRemoveElementByIndex_Internal(int32 InIndex);
+	/* Server only */
+	virtual bool TrySwapElement_Internal(int32 OldIndex, int32 NewIndex);
+
+protected:
+	AActor* AddElement(AActor* Element, int32 InIndex);
+	void RemoveElement(int32 InIndex);
+	
+	UFUNCTION(Server, Reliable)
+	void ServerTryAddElement(AActor* Element, int32 InIndex);
+	UFUNCTION(Server, Reliable)
+	void ServerTryRemoveElement(AActor* Element, int32 InIndex);
+	UFUNCTION(Server, Reliable)
+	void ServerTryRemoveElementByIndex(int32 InIndex);
+	UFUNCTION(Server, Reliable)
+	void ServerTrySwapElement(int32 OldIndex, int32 NewIndex);
+	
+	UFUNCTION()
+	void OnRep_Elements(TArray<AActor*> OldElements);
+
+	int FindFirstAvailableIndex();
+	//~Container
+};
